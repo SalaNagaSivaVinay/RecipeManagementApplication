b2f84a809eeba8f9af12d47700f1b582de9e98c9
SalaNagaSivaVinay <nagasivavinay.s@gmail.com>
2025-03-09 23:58:03 +0530

    Deploy updates

--- RecipeApp/frontend/vite.config.js
+++ RecipeApp/frontend/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
 export default defineConfig({
-  plugins: [react()],
-  base: "/", // Change from "./" to "/"
-  build: {
-    outDir: "dist",
-  },
-});
+    base: "./",
+    build: {
+      outDir: "dist",
+    },
+  });
+  
