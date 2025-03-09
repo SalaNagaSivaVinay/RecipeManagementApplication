bad55132abdbcf311e4030cb95cc6505219afca2
SalaNagaSivaVinay <nagasivavinay.s@gmail.com>
2025-03-10 00:21:43 +0530

    Fix Vercel config

--- RecipeApp/frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: './', // Ensures the base path is set correctly for relative URLs
-  build: {
-    outDir: 'dist', // Output directory for the production build
-  },
-});
